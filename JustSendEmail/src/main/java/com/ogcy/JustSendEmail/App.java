838cb30147cc2210b74f6677684b2a43a6b90455
talhamdi <stlmhibg30A+>
2018-10-25 15:12:43 +0300

    after add image class

--- JustSendEmail/src/main/java/com/ogcy/JustSendEmail/App.java
+++ JustSendEmail/src/main/java/com/ogcy/JustSendEmail/App.java
@@ -6,6 +6,22 @@ import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import com.ogcy.JustSendEmail.*;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class App 
 {
@@ -31,13 +47,15 @@ public class App
 		};
 		Session session = Session.getInstance(props, auth);
 		
-		EmailUtil.sendEmail(session, toEmail,"TLSEmail Testing Subject", "TLSEmail Testing Body");
-		
-	
-		
+		//EmailUtil.sendEmail(session, toEmail,"TLSEmail Testing Subject", "TLSEmail Testing Body");	
+		 EmailUtil.sendImageEmail(session, toEmail,"SSLEmail Testing Subject with Image", "SSLEmail Testing Body with Image");
 	}
+    
+ 
+    
+}
 
-	}
+
 
 
     	
